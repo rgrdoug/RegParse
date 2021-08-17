34967c67a60831d6f96d01a694a3d714f4ab7dc0
doug <rgrdoug@gmail.com>
2021-08-17 17:59:10 -0500

    Added code to read multiple file and folder paths. Also defined main function and RegParser class.

--- RegParse.py
+++ RegParse.py
@@ -7,32 +7,40 @@ from prettytable import PrettyTable     # To create a pretty table
 
 from regipy.registry import RegistryHive # Python library to convert offline registry files to readable format 
 
+class RegParser: 
+    def __init__(self, hiveFilePath, regFolder):
+        self.hiveFilePath = hiveFilePath
+        self.regFolder = regFolder
+        self.keyPaths = []
+        self.parseHiveFile()
+        
+    def parseHiveFile(self):
+        with open(self.hiveFilePath, 'r') as hiveFile:
+            for line in hiveFile:
+                self.keyPaths.append(line.strip())
+                
+    def parse(self):
+        pass
+    
+    
+        
+        
+def main():
+    if len(sys.argv) !=3:
+        print("Usage: python RegParse.py hiveFilePath regFolder")
+        print("\t hiveFilePath: ")                          #
+        print("\t regFolder: ")
+        exit(1)                                             #exits with an error code of 1 to indicate not being run correctly
+    hiveFilePath = sys.argv[1]                                  #A list of command line arguments, and used to have user input data into program
+    regFolder = sys.argv[2]
+     
+    regParser = RegParser(hiveFilePath, regFolder)
+    regParser.parse()
+    
+    print(regParser.regFolder)
+    print(regParser.hiveFilePath)    
+    
+    
+if __name__ == "__main__":
+    main()
 
-if len(sys.argv) !=2:
-    print("Please enter one file path as command line argument")
-    exit(1)                                             #exits with an error code of 1 to indicate not being run correctly
-
-hiveFile = sys.argv[1]                                   #A list of command line arguments, and used to have user input data into program
-
-#Need to argparse this so that the user can input which file they want 
-
-
-reg = RegistryHive(hiveFile)
-
-# Iterate over a registry hive recursively:
-for entry in reg.recurse_subkeys(as_json=True):
-    print(entry)
-
-## Iterate over a key and get all subkeys and their modification time:
-#for sk in reg.get_key('Software').get_subkeys():
-    #print(sk.name, convert_wintime(sk.header.last_modified).isoformat())
-
-## Get values from a specific registry key:
-#reg.get_key('Software\Microsoft\Internet Explorer\BrowserEmulation').get_values(as_json=True)
-
-## Use plugins:
-#from regipy.plugins.ntuser.ntuser_persistence import NTUserPersistencePlugin
-#NTUserPersistencePlugin(reg, as_json=True).run()
-
-## Run all supported plugins on a registry hive:
-#run_relevant_plugins(reg, as_json=True)
